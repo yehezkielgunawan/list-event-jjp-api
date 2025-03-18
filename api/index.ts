79ac4bfbdd4c7045ed58a8d962cb75e4022e04c8
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2025-03-18 20:16:39 +0700

    refactor(api/index.ts): improve type imports and clean up code formatting

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,4 @@
-import express, { Express, Request, Response } from "express";
+import express, { type Express, type Request, type Response } from "express";
 import { google } from "googleapis";
 import dotenv from "dotenv";
 
@@ -8,54 +8,57 @@ dotenv.config();
 const sheet = google.sheets({ version: "v4", auth: process.env.KEY });
 
 app.get("/", async (req: Request, res: Response) => {
-  // recieve query param named city
-  const city = req.query.city as string;
-  const eventList = await sheet.spreadsheets.values
-    .get({
-      spreadsheetId: process.env.SPREADSHEET_ID,
-      range: "Event",
-    })
-    .then((response: any) => {
-      //   remove the first two index because it's only label, not the data
-      return response.data.values?.slice(2).map((col: any[]) => {
-        return {
-          event_date: col[0],
-          event_name: col[4],
-          event_city: col[3],
-          event_location: col[2],
-          event_info_link: col[6],
-        };
-      });
-    });
-  res
-    .status(200)
-    .send(
-      city
-        ? eventList.filter((event: any) =>
-            event.event_city.toLowerCase().includes(city.toLocaleLowerCase())
-          )
-        : eventList
-    );
+	// recieve query param named city
+	const city = req.query.city as string;
+	const eventList = await sheet.spreadsheets.values
+		.get({
+			spreadsheetId: process.env.SPREADSHEET_ID,
+			range: "Event",
+		})
+		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+		.then((response: any) => {
+			//   remove the first two index because it's only label, not the data
+			// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+			return response.data.values?.slice(2).map((col: any[]) => {
+				return {
+					event_date: col[0],
+					event_name: col[4],
+					event_city: col[3],
+					event_location: col[2],
+					event_info_link: col[6],
+				};
+			});
+		});
+	res.status(200).send(
+		city
+			? // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+				eventList.filter((event: any) =>
+					event.event_city.toLowerCase().includes(city.toLocaleLowerCase()),
+				)
+			: eventList,
+	);
 });
 
 app.get("/cities", async (req: Request, res: Response) => {
-  // return unique cities
-  const eventList = await sheet.spreadsheets.values
-    .get({
-      spreadsheetId: process.env.SPREADSHEET_ID,
-      range: "Event",
-    })
-    .then((response: any) => {
-      return response.data.values?.slice(2).map((col: any[]) => {
-        return col[3];
-      });
-    });
-  const uniqueCities = [...new Set(eventList)];
-  res.status(200).send(uniqueCities);
+	// return unique cities
+	const eventList = await sheet.spreadsheets.values
+		.get({
+			spreadsheetId: process.env.SPREADSHEET_ID,
+			range: "Event",
+		})
+		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+		.then((response: any) => {
+			// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+			return response.data.values?.slice(2).map((col: any[]) => {
+				return col[3];
+			});
+		});
+	const uniqueCities = [...new Set(eventList)];
+	res.status(200).send(uniqueCities);
 });
 
 app.listen(port, () => {
-  console.log(`Server is running now!`);
+	console.log("Server is running now!");
 });
 
 module.exports = app;
